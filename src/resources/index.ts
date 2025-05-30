10fc49ef30b47933cb40b4dc587cc31d848fc7bf
Rohit Gupta <rohitgr1998@gmail.com>
2025-05-30 22:46:12 +0530

    restart

--- src/resources/index.ts
+++
@@ -1,9 +0,0 @@
-// File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-export {
-  Chat,
-  type ChatCompletionsResponse,
-  type ChatListModelsResponse,
-  type ChatListModelsInternalResponse,
-  type ChatCompletionsParams,
-} from './chat';
